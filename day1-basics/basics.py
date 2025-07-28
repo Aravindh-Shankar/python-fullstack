0fa23f49d1c135ace1f7a08ee707d8f30d88d9e8
Aravindh-Shankar <aravindh.vel@agiletribers.com>
2025-07-28 12:21:29 +0530

    python new

---
+++ day1-basics/basics.py
@@ -0,0 +1,11 @@
+print("hello, World!")
+
+name = input("what is your name?")
+print("hello" + " " + name + "!")
+
+x = 1
+y = 3
+print("x + y = " + str(x + y))
+
+age = input("whats your age?")
+print("your age is", int(age))
